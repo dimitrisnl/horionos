2bdbb8b6659f721f8c580a42826f6bc1af9215e0
Dimitrios Lytras <dnlytras@gmail.com>
2024-06-07 16:45:51 +0300

    Small design changes

--- lib/horionos_web/controllers/page_controller.ex
+++ lib/horionos_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule HorionosWeb.PageController do
   use HorionosWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home)
   end
 end
